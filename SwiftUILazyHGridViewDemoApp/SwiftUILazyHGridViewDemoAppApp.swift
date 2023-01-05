e48f2774f8efcae06717f6efa2f4b46a5c9ffb9b
ryota <k014c1106@it-neec.jp>
2023-01-05 23:17:23 +0900

    Initial Commit

---
+++ SwiftUILazyHGridViewDemoApp/SwiftUILazyHGridViewDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILazyHGridViewDemoAppApp.swift
+//  SwiftUILazyHGridViewDemoApp
+//
+//  Created by ryota on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILazyHGridViewDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
